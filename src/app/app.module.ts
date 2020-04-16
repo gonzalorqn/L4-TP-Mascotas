33687465cdcc96aad26e975d86215b801fb5be1e
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2020-04-16 06:17:56 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CargaMascotaComponent } from './componentes/carga-mascota/carga-mascota.component';
+import { MascotaComponent } from './componentes/mascota/mascota.component';
+import { ListadoComponent } from './componentes/listado/listado.component';
+
+import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './componentes/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CargaMascotaComponent,
+    MascotaComponent,
+    ListadoComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
