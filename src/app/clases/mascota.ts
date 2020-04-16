88fba7d4c3cf40712f3aa96021d192bb3b035bf1
gonzalorqn <36246342+gonzalorqn@users.noreply.github.com>
2020-04-16 06:18:22 -0300

    Add files via upload

---
+++ src/app/clases/mascota.ts
@@ -0,0 +1,11 @@
+export class Mascota {
+    nombre: string;
+    imagen: string;
+    tipo: string;
+
+    constructor(nombre: string, imagen: string, tipo: string){
+        this.nombre = (nombre);
+        this.imagen = (imagen);
+        this.tipo = (tipo);
+    }
+}
